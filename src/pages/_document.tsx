ad25663604a3fafc54a42a3c867d10e1f632dd7d
TaciTa <79896328+00TaciTa00@users.noreply.github.com>
2024-12-08 00:26:12 +0900

    title 정의

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <title>불타는 마음으로</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
